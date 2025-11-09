e97e084c822d2adf5f1d33ebae5189ab7647e2ce
DominicDev <s0l0estudi4r@gmail.com>
2025-11-09 22:01:32 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KINGS OF LEON';
+export let nameSongs = [
+	"USE SOMEBODY", /*1*/
+	"FIND ME", /*2*/
+	"SEX ON FIRE", /*3*/
+	"THE FACE", /*4*/
+	"TEMPLE", /*5*/
+	"THE IMMORTALS", /*6*/
+	"PYRO", /*7*/
+	"THE END", /*8*/
+	"OVER", /*9*/
+	"BEAUTIFUL WAR", /*10*/
+	"WAIT FOR ME", /*11*/
+	"SUPERSOAKER", /*12*/
+	"I WANT YOU", /*13*/
+	"REVELRY", /*14*/
+	"CLOSER", /*15*/
+	"COMEBACK STORY", /*16*/
+	"LAST MILE HOME", /*17*/
+	"MANHATTAN", /*18*/
+	"ARIZONA", /*19*/
+	"ON CALL", /*20*/
+]
